83dd0fe1a23613114b1789a7662e7ebdbdeb8b0d
boliqq07 <986798607@qq.com>
2021-05-20 23:47:59 +0800

    base_graph atom add

--- setup.py
+++ setup.py
@@ -17,13 +17,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
 
     name='featurebox',
-    version='0.0.941',
+    version='0.0.942',
     keywords=['features', "combination", "selection"],
     description='This is an box contains tools for machine learning.'
                 'Some of code are non-originality, just copy for use. All the referenced code are marked,'
                 'details can be shown in their sources',
     install_requires=['pandas', 'numpy', 'sympy', 'scipy', 'scikit-learn', 'joblib', 'matplotlib',
-                      'requests', 'tqdm', 'six', "mgetool", "pymatgen", "deap"],
+                      'requests', 'tqdm', 'six', "mgetool", "pymatgen", "deap", "numba", "ase"],
     include_package_data=True,
     author='wangchangxin',
     author_email='986798607@qq.com',
